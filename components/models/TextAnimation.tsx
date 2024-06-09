30432b650470726eec00d985309f6033eab203ab
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-06-09 16:56:06 +0100

    style: :lipstick: model modifications

--- components/models/TextAnimation.tsx
+++ components/models/TextAnimation.tsx
@@ -10,13 +10,13 @@ function TextAnimation() {
 
   useFrame(() => {
     mesh.current.rotation.x += 0.01;
-    mesh.current.rotation.y += 0.01;
+    mesh.current.rotation.y += 0.016;
   });
 
   const materialProps = useControls({
-    thickness: { value: 0.2, min: 0, max: 3, step: 0.05 },
-    roughness: { value: 0, min: 0, max: 1, step: 0.1 },
-    transmission: { value: 1, min: 0, max: 3, step: 0.1 },
+    thickness: { value: 0.3, min: 0, max: 3, step: 0.05 },
+    roughness: { value: 0.1, min: 0, max: 1, step: 0.1 },
+    transmission: { value: 0.9, min: 0, max: 3, step: 0.1 },
     ior: { value: 1.2, min: 0, max: 3, step: 0.1 },
     ChromaticAberration: { value: 0.02, min: 0, max: 1 },
     backside: { value: true },
@@ -24,8 +24,8 @@ function TextAnimation() {
 
   return (
     <group scale={viewport.width / 6.5}>
-      <Text fontSize={0.6} position={[0, 0, -0.5]}>
-        HELLO FRIENDS
+      <Text fontSize={0.7} position={[0, 0, -0.5]}>
+        Awsome 3D
       </Text>
       <mesh ref={mesh} {...nodes.Base}>
         <MeshTransmissionMaterial {...materialProps} />
